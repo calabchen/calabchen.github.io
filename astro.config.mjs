c643208c5cb68319657f1e73df78cf77faa83891
CalabChen <162004123@qq.com>
2025-02-13 13:13:42 +0800

    删除关于playwright的一切，取消对mermaid语法的支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,6 @@ import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
-import remarkMermaid from "remark-mermaidjs";
 
 
 // https://astro.build/config
@@ -75,7 +74,6 @@ export default defineConfig({
       remarkDirective,
       remarkSectionize,
       parseDirectiveNode,
-      remarkMermaid,
     ],
     rehypePlugins: [
       rehypeKatex,
